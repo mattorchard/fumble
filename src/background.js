913623e2d1694ce3a36cd30edb354e390e1c9175
morch028 <morch028@uottawa.ca>
2019-12-04 18:10:03 -0500

    Adds background script for handling save requests

--- src/background.js
+++ src/background.js
@@ -1,1 +1,28 @@
-console.log("Hello from background script");
+import { saveText } from "./repositories/text-repository";
+
+const messageHandlers = {
+  requestSaveText: saveText,
+};
+
+chrome.runtime.onMessage.addListener(message => {
+  // Anything other than the message type is considered payload
+  const { messageType, ...payload } = message;
+
+  if (!(messageType in messageHandlers)) {
+    console.warn(
+      "No message handler for type",
+      messageType,
+      "With payload",
+      payload
+    );
+    return;
+  }
+
+  try {
+    messageHandlers[messageType].call(null, payload);
+  } catch (error) {
+    console.error("Failed to handle message", messageType, error, payload);
+  }
+
+  return true;
+});
