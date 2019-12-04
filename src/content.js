55b85d9c1b64ec9a2e2b3c50836907e373e8b9f2
morch028 <morch028@uottawa.ca>
2019-12-04 18:11:08 -0500

    Adds content script for requesting save after input edited

--- src/content.js
+++ src/content.js
@@ -1,1 +1,47 @@
-console.log("Hello from content script");
+import getElementUid from "./helpers/getElementUid";
+
+const SAVE_AFTER_DELAY = 500;
+const SAVE_AFTER_LENGTH = 72;
+
+const saveText = ({ text, elementUid, href }) => {
+  if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+    chrome.runtime.sendMessage({
+      messageType: "requestSaveText",
+      text,
+      elementUid,
+      href,
+    });
+  } else {
+    console.warn("Unable to send message, runtime undefined");
+  }
+};
+
+const saveTextDebounced = (() => {
+  let timeoutId;
+  let prevElementUid;
+  let prevHref;
+  return ({ text, elementUid, href }) => {
+    if (timeoutId && prevElementUid === elementUid && prevHref === href) {
+      clearTimeout(timeoutId);
+    }
+    prevElementUid = elementUid;
+    prevHref = href;
+    timeoutId = setTimeout(
+      () => saveText({ text, elementUid, href }),
+      SAVE_AFTER_DELAY
+    );
+  };
+})();
+
+const globalInputHandler = event => {
+  const { value } = event.target;
+  if (value && value.length >= SAVE_AFTER_LENGTH) {
+    saveTextDebounced({
+      text: value,
+      elementUid: getElementUid(event.target),
+      href: window.location.href,
+    });
+  }
+};
+
+document.addEventListener("input", globalInputHandler);
