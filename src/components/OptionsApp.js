261a44367fb83028eab6c4fa537aa10268027a1e
morch028 <morch028@uottawa.ca>
2019-12-04 23:01:52 -0500

    Setup styles for options page

---
+++ src/components/OptionsApp.js
@@ -0,0 +1,5 @@
+import { render, h } from "preact";
+
+const App = <h1>Hello Options!</h1>;
+
+render(App, document.getElementById("root"));
--- src/options.js
+++
@@ -1,5 +0,0 @@
-import {render, h} from "preact";
-
-const App = <h1>Hello Options!</h1>;
-
-render(App, document.getElementById("root"));
